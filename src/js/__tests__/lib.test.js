3c397780079ebbcbede693f99184d77dd498347f
ilsur <ilsur>
2019-05-03 19:25:40 +0300

    Fixed #1, Closed #2, Travis CI added

--- src/js/__tests__/lib.test.js
+++ src/js/__tests__/lib.test.js
@@ -6,13 +6,58 @@ test('should return 7 rolls', () => {
     const roomHeight = 275;
     const wallpaperWidth = 106;
     const wallpaperLength = 1000;
-    const firstDockingRatio = 0;
+    const firstDockingRatio = 64;
     const secondDockingRatio = 0;
-    const  expected = 7;
+    const expected = 7;
 
-    const result = wallpaperCalc(roomWidth,roomLength,roomHeight,wallpaperWidth,wallpaperLength,
-        firstDockingRatio,secondDockingRatio);
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
 
     expect(result).toBe(expected);
 })
 
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 275;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 0;
+    const secondDockingRatio = 0;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 275;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 64;
+    const secondDockingRatio = 32;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 240;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 64;
+    const secondDockingRatio = 32;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
--- src/js/lib.js
+++ src/js/lib.js
@@ -2,16 +2,18 @@ export function wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth,
                               firstDockingRatio, secondDockingRatio) {
     const addRoomHeight = roomHeight + 10;
     const perimeter = 2 * roomWidth + 2 * roomLength;
-    const numberOfCanvas =  Math.ceil(perimeter / wallpaperWidth);
+    const numberOfCanvas = Math.ceil(perimeter / wallpaperWidth);
     let canvasInRolls;
     if (firstDockingRatio === 0) {
         canvasInRolls = Math.floor(wallpaperLength / addRoomHeight);
     } else {
-        let canvasLength =  Math.ceil(addRoomHeight / firstDockingRatio) * firstDockingRatio;
+        let canvasLength = Math.ceil(addRoomHeight / firstDockingRatio) * firstDockingRatio;
         if (secondDockingRatio > 0) {
             if (addRoomHeight < canvasLength - secondDockingRatio) {
                 canvasLength = canvasLength - secondDockingRatio;
-            } else canvasLength = canvasLength + secondDockingRatio;
+            } else {
+                canvasLength = canvasLength + secondDockingRatio;
+            }
         }
         canvasInRolls = Math.floor(wallpaperLength / canvasLength);
     }
