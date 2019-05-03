86f227553a5de0346890510fbd66004a645dbaac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-03 16:31:11 +0000

    Deploy ShaidVL/homework_walpaper_calc to github.com/ShaidVL/homework_walpaper_calc.git:gh-pages

---
+++ js/__tests__/lib.test.js
@@ -0,0 +1,63 @@
+import {wallpaperCalc} from "../lib.js";
+
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 275;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 64;
+    const secondDockingRatio = 0;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
+
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 275;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 0;
+    const secondDockingRatio = 0;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 275;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 64;
+    const secondDockingRatio = 32;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
+test('should return 7 rolls', () => {
+    const roomWidth = 500;
+    const roomLength = 600;
+    const roomHeight = 240;
+    const wallpaperWidth = 106;
+    const wallpaperLength = 1000;
+    const firstDockingRatio = 64;
+    const secondDockingRatio = 32;
+    const expected = 7;
+
+    const result = wallpaperCalc(roomWidth, roomLength, roomHeight, wallpaperWidth, wallpaperLength,
+        firstDockingRatio, secondDockingRatio);
+
+    expect(result).toBe(expected);
+})
---
+++ js/app.js
@@ -0,0 +1,5 @@
+import {wallpaperCalc} from "./lib.js";
+
+const rollsNumber = wallpaperCalc(500, 600, 275, 106, 1000,
+    64, 0);
+console.log(rollsNumber);
